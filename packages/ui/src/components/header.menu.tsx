92646013086d476ef3a30db4b7820693e26e6519
Sajidh Farook <sajidh@begood.solutions>
2025-06-24 14:54:03 +0100

    refactor: improves header.menu.tsx

--- packages/ui/src/components/header.menu.tsx
+++ packages/ui/src/components/header.menu.tsx
@@ -1,17 +1,19 @@
-import {Tabs, Text} from '@mantine/core';
+import {Container, Tabs, Text} from '@mantine/core';
 import {useLocation, useNavigate} from 'react-router';
 
 export const HeaderMenu = () => {
-	const navigate = useNavigate();
-	const { pathname } = useLocation();
+    const navigate = useNavigate();
+    const {pathname} = useLocation();
 
-	return (
-		<Tabs variant={'pills'} defaultValue={pathname.slice(1)} value={pathname.slice(1)}>
-			<Tabs.List m={5}>
-				<Tabs.Tab value="add-expense" onClick={() => navigate('/')} style={{ borderRadius: '10px' }}>
-					<Text fz={14}> Home </Text>
-				</Tabs.Tab>
-			</Tabs.List>
-		</Tabs>
-	);
+    return (
+        <Container size="lg">
+            <Tabs variant="pills" defaultValue="/" value={pathname}>
+                <Tabs.List my="md">
+                    <Tabs.Tab value="/" onClick={() => navigate('/')} style={{borderRadius: '8px'}}>
+                        <Text fz={14}> Home </Text>
+                    </Tabs.Tab>
+                </Tabs.List>
+            </Tabs>
+        </Container>
+    );
 };
