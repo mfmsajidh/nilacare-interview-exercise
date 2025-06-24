729becf2abfdf017bdc92d9d17d301d0b407ccab
Sajidh Farook <sajidh@begood.solutions>
2025-06-24 15:05:35 +0100

    refactor: improves App.tsx and header.menu.tsx

--- packages/ui/src/App.tsx
+++ packages/ui/src/App.tsx
@@ -1,36 +1,31 @@
 import {ActionIcon, AppShell, Group, Image} from '@mantine/core';
-import {client} from '@nila/client/src/client.gen.ts';
 import {IconLogout} from '@tabler/icons-react';
 import {Outlet, useNavigate} from 'react-router';
 import Logo from '../public/logo.png';
 import {HeaderMenu} from './components/header.menu.tsx';
 
-client.setConfig({
-	baseUrl: import.meta.env.VITE_NILA_API_URL,
-});
-
 export function App() {
 
-	const navigate = useNavigate();
+    const navigate = useNavigate();
 
-	return (
-		<AppShell header={{height: 60}} padding="md">
-			<AppShell.Header px={'xs'}>
-				<Group justify={'space-between'} h={'100%'}>
-					<Image src={Logo} visibleFrom={'sm'} maw={100} onClick={() => navigate('/home')}/>
-					<Group gap={'xs'}>
-						<HeaderMenu/>
+    return (
+        <AppShell header={{height: 60}} padding="md">
+            <AppShell.Header px={'xs'}>
+                <Group justify={'space-between'} h={'100%'}>
+                    <Image src={Logo} visibleFrom={'sm'} maw={100} onClick={() => navigate('/')}/>
+                    <Group gap={'xs'}>
+                        <HeaderMenu/>
 
-						<ActionIcon size={'sm'} variant={'transparent'}>
-							<IconLogout/>
-						</ActionIcon>
-					</Group>
-				</Group>
-			</AppShell.Header>
+                        <ActionIcon size={'sm'} variant={'transparent'}>
+                            <IconLogout/>
+                        </ActionIcon>
+                    </Group>
+                </Group>
+            </AppShell.Header>
 
-			<AppShell.Main>
-				<Outlet/>
-			</AppShell.Main>
-		</AppShell>
-	);
+            <AppShell.Main>
+                <Outlet/>
+            </AppShell.Main>
+        </AppShell>
+    );
 }
--- packages/ui/src/components/header.menu.tsx
+++ packages/ui/src/components/header.menu.tsx
@@ -1,19 +1,17 @@
-import {Container, Tabs, Text} from '@mantine/core';
+import {Tabs, Text} from '@mantine/core';
 import {useLocation, useNavigate} from 'react-router';
 
 export const HeaderMenu = () => {
     const navigate = useNavigate();
-    const {pathname} = useLocation();
+    const { pathname } = useLocation();
 
     return (
-        <Container size="lg">
-            <Tabs variant="pills" defaultValue="/" value={pathname}>
-                <Tabs.List my="md">
-                    <Tabs.Tab value="/" onClick={() => navigate('/')} style={{borderRadius: '8px'}}>
-                        <Text fz={14}> Home </Text>
-                    </Tabs.Tab>
-                </Tabs.List>
-            </Tabs>
-        </Container>
+        <Tabs variant={'pills'} defaultValue={pathname.slice(1)} value={pathname.slice(1)}>
+            <Tabs.List m={5}>
+                <Tabs.Tab value="/" onClick={() => navigate('/')} style={{ borderRadius: '10px' }}>
+                    <Text fz={14}> Home </Text>
+                </Tabs.Tab>
+            </Tabs.List>
+        </Tabs>
     );
 };
